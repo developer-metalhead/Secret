102258ab050415136144a90e1ae8f9ef9a7f1df0
developer-metalhead <satwikshah2000@gmail.com>
2023-07-16 02:11:04 +0530

    Changing connect

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const app = express();
 mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}, () => {
-  console.log("Connected to mongoose successfully")
 });
 
 
