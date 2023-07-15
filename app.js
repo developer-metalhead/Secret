9aaf793cc84964bec2ef21cf195d36fd64a318eb
developer-metalhead <satwikshah2000@gmail.com>
2023-07-16 03:26:40 +0530

    Changing connect

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://gray-gentle-tick.cyclic.app/auth/google/secrets",
+      callbackURL: "https://gray-gentle-tick.cyclic.app/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
